d00c7d24e59df5ad2bf2c713f6d2377d1c10e8ec
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-03-16 19:52:16 -0400

    adding default code in the editor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,38 @@ const { asciiToBin } = convert;
 const { sendBtn, STARTBITS, ENDBITS, _32TIMINGBITS } = constants;
 
 const editor = monaco.editor.create(document.getElementById('container'), {
+  value: ` #include <stdio.h>
+  #include <stdbool.h>
+  #include <string.h>
+  #include <ctype.h>
+  
+  bool is_isogram(char phrase[])
+  {
+    if (phrase == NULL)
+    {
+      return false;
+    }
+  
+    unsigned int i;
+    int letters_seen[26];
+    for (i = 0; i < strlen(phrase); i++)
+    {
+      char letter = toupper(phrase[i]);
+      if (isalpha(letter))
+      {
+        if (letter == letters_seen[letter - 97])
+        {
+          return false;
+        }
+        else
+        {
+          letters_seen[letter - 97] = letter;
+        }
+      }
+    }
+  
+    return true;
+  }`,
   language: 'cpp',
   theme: "vs-dark",
 });
