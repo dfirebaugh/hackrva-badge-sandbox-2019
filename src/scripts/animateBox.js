97a53a9fd762bab819f22926780a02fb11d11e14
dfirebaugh <dafirebaugh@gmail.com>
2019-03-17 10:29:47 -0400

    changing 0 symbol to transition from 1 to 0 changing 1 to transition from 0 to 1

--- src/scripts/animateBox.js
+++ src/scripts/animateBox.js
@@ -1,11 +1,11 @@
-import convert from './convert';
-import constants from './constants';
-import logger from './logger';
+import convert from "./convert";
+import constants from "./constants";
+import logger from "./logger";
 const { canvas, ctx, textBox } = constants;
 const { binToAscii } = convert;
 
 const updateStatus = status => {
-  document.getElementById('status').textContent = status;
+  document.getElementById("status").textContent = status;
 };
 
 // Animation
@@ -13,39 +13,47 @@ const updateStatus = status => {
 let start = null;
 let index = 0;
 
-export default (binary, transmitMsg) => new Promise(function (resolve, reject) {
-  const binaryDiv = document.createElement('div');
-  binaryDiv.value = binary;
-  document.getElementById('bits').appendChild(binaryDiv);
+export default (binary, transmitMsg) =>
+  new Promise(function(resolve, reject) {
+    const binaryDiv = document.createElement("div");
+    binaryDiv.value = binary;
+    document.getElementById("bits").appendChild(binaryDiv);
 
-  updateStatus(transmitMsg);
-  logger(transmitMsg);
-  logger("Binary: ", binary);
-  logger("Value converted back: ", binToAscii(binary));
-  logger("length: ", binary.length);
-  const updateProgress = progress => {
-    const progressBar = document.getElementById('progressBar');
-    // console.log(progressBar);
-    progressBar.setAttribute("style", `width: ${progress}%`);
-  };
-  const drawFrame = () => {
-    const fillColor = binary[index] == 0 ? "red" : "black";
+    updateStatus(transmitMsg);
+    logger(transmitMsg);
+    logger("Binary: ", binary);
+    logger("Value converted back: ", binToAscii(binary));
+    logger("length: ", binary.length);
+    const updateProgress = progress => {
+      const progressBar = document.getElementById("progressBar");
+      // console.log(progressBar);
+      progressBar.setAttribute("style", `width: ${progress}%`);
+    };
+    const drawFrame = () => {
+      if (binary[index] == 0) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      } else {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      }
+      index++;
+    };
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      if (index < binary.length) {
+        drawFrame();
+        updateProgress((100 * index) / binary.length);
+        window.requestAnimationFrame(step);
+      } else {
+        index = 0;
+        resolve();
+      }
+    }
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = fillColor;
-    index++;
-  };
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    if (index < binary.length) {
-      drawFrame();
-      updateProgress((100 * index) / binary.length);
-      window.requestAnimationFrame(step);
-    }
-    else {
-      index = 0;
-      resolve();
-    }
-  }
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  window.requestAnimationFrame(step);
-});
+    window.requestAnimationFrame(step);
+  });
