fa2923d9e65b93199c17671971f092df1b6a9ccf
dfirebaugh <dafirebaugh@gmail.com>
2019-03-17 10:32:37 -0400

    transmitting hash -- removing timing bits

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,61 +1,64 @@
-import '../styles/index.scss';
-import animateBox from './animateBox';
-import convert from './convert';
-import constants from './constants';
-import * as monaco from 'monaco-editor';
-import logger from './logger';
+import "../styles/index.scss";
+import animateBox from "./animateBox";
+import convert from "./convert";
+import constants from "./constants";
+import * as monaco from "monaco-editor";
+import logger from "./logger";
+import md5 from "md5";
 
 const { asciiToBin } = convert;
-const { sendBtn, STARTBITS, ENDBITS, _32TIMINGBITS } = constants;
+const { sendBtn, STARTBITS, ENDBITS } = constants;
 
-const editor = monaco.editor.create(document.getElementById('container'), {
-  value: ` #include <stdio.h>
-  #include <stdbool.h>
-  #include <string.h>
-  #include <ctype.h>
-  
-  bool is_isogram(char phrase[])
+const editor = monaco.editor.create(document.getElementById("container"), {
+  value: `#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+bool is_isogram(char phrase[])
+{
+  if (phrase == NULL)
   {
-    if (phrase == NULL)
+    return false;
+  }
+
+  unsigned int i;
+  int letters_seen[26];
+  for (i = 0; i < strlen(phrase); i++)
+  {
+    char letter = toupper(phrase[i]);
+    if (isalpha(letter))
     {
-      return false;
-    }
-  
-    unsigned int i;
-    int letters_seen[26];
-    for (i = 0; i < strlen(phrase); i++)
-    {
-      char letter = toupper(phrase[i]);
-      if (isalpha(letter))
+      if (letter == letters_seen[letter - 97])
       {
-        if (letter == letters_seen[letter - 97])
-        {
-          return false;
-        }
-        else
-        {
-          letters_seen[letter - 97] = letter;
-        }
+        return false;
+      }
+      else
+      {
+        letters_seen[letter - 97] = letter;
       }
     }
-  
-    return true;
-  }`,
-  language: 'cpp',
-  theme: "vs-dark",
+  }
+
+  return true;
+}`,
+  language: "cpp",
+  theme: "vs-dark"
 });
 
-sendBtn.addEventListener('click', () => {
+sendBtn.addEventListener("click", () => {
   logger("value: ", editor.getValue());
-  animateBox(_32TIMINGBITS, "transmitting 32 timing bits")
-    .then(() => {
-      animateBox(STARTBITS, "transmitting start bits")
-        .then(() => {
-          animateBox(asciiToBin(editor.getValue()), "transmitting main payload")
-            .then(() => {
-              animateBox(ENDBITS, "transmitting end bits");
-            });
+  logger("hash: ", md5(editor.getValue()));
+  animateBox(STARTBITS, "transmitting start bits").then(() => {
+    animateBox(asciiToBin(md5(editor.getValue())), "transmitting hash").then(
+      () => {
+        animateBox(
+          asciiToBin(editor.getValue()),
+          "transmitting main payload"
+        ).then(() => {
+          animateBox(ENDBITS, "transmitting end bits");
         });
-    });
-}
-);
+      }
+    );
+  });
+});
